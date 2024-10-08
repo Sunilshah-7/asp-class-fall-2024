0733d8921bb7cc0e8ceadd3ec75a82ce3c204e1b
SUNIL Shah <sunilshah@SUNILs-MacBook-Air.local>
2024-10-08 12:20:41 -0400

    Fixing edge cases for hw5

--- hw_5.py
+++ hw_5.py
@@ -9,6 +9,7 @@ class Shape:
 
     def calculate_area(self):
         pass
+    
 
 # Triangle class that inherits from Shape
 class Triangle(Shape):
@@ -54,18 +55,29 @@ class Parallelogram(Shape):
         return self.base * self.height
 
 #  Validation of input for any shape
-def validate_input(params):
+def validate_input(params, expected_length):
     try:
         new_params = [float(x) for x in params]
+        if len(new_params) != expected_length:
+            print("Input parameters are incorrect. Please enter correct number of parameters.")
+            return None
         for x in new_params:
             if x <= 0:
-                print("Input parameters are incorrect. Please enter positive numers only.")
+                print("Input parameters are incorrect. Please enter positive numbers only.")
                 print()
-                return None
+                return None   
         return new_params
     except ValueError:
         print("Input parameters are incorrect. Please enter valid numeric input.")
         return None
+
+def get_validated_input(prompt, expected_length):
+    while True:
+        params = input(prompt)
+        validated_params = validate_input(params.split(), expected_length)
+        if validated_params and len(validated_params) == expected_length:
+            return validated_params
+
 
 # Main Menu to select the shape
 def main_menu():
@@ -83,59 +95,38 @@ def main_menu():
         option = input("Enter your choice:")
         if option == 'a':
             print("We are calculating area of Triangle.")
-            params = input("Please enter the base and height of the triangle separated by space:")
-            validated_params = validate_input(params.split())
-            print("Validate params",validated_params)
+            validated_params = get_validated_input("Please enter the base and height of triangle separated by space (in ft):", 2)
+            triangle = Triangle(validated_params[0], validated_params[1])
+            print("The area of triangle with base {} and height {} is {} sq ft.".format(triangle.base, triangle.height, triangle.calculate_area()))
             print()
-            if validated_params and len(validated_params) == 2:
-                    triangle = Triangle(validated_params[0], validated_params[1])
-                    print("The area of triangle with base {} and height {} is {}".format(triangle.base, triangle.height, triangle.calculate_area()))
-            else:
-                print("Please enter the correct number of parameters.")
-                print()
 
         elif option == 'b':
             print("We are calculating the area of Rectangle.")
-            params = input("Please enter the length and width of rectangle separated by space:")
-            validated_params = validate_input(params.split())
-            if validated_params and len(validated_params) == 2:
-                rectangle = Rectangle(validated_params[0], validated_params[1])
-                print("The area of rectangle with length {} and width {} is {}".format(rectangle.length, rectangle.width, rectangle.calculate_area()))
-            else:
-                print("Please enter the correct number of parameters.")
+            validated_params = get_validated_input("Please enter the length and width of rectangle separated by space (in ft):", 2)
+            rectangle = Rectangle(validated_params[0], validated_params[1])
+            print("The area of rectangle with length {} and width {} is {} sq ft.".format(rectangle.length, rectangle.width, rectangle.calculate_area()))
+            print()
 
         elif option == 'c':
             print("We are calculating the area of Square.")
-            params = input("Please enter the length of the square:")
-            validated_params = validate_input(params.split())
-            if validated_params and len(validated_params) == 1:
-                square = Square(validated_params[0])
-                print("The area of square with length {} is {}".format(square.length, square.calculate_area()))
-            else:
-                print("Please enter the correct number of parameters.")
-                print()
+            validated_params = get_validated_input("Please enter the length of the square (in ft):", 1)
+            square = Square(validated_params[0])
+            print("The area of square with length {} is {} sq ft.".format(square.length, square.calculate_area()))
+            print()
 
         elif option == 'd':
             print("We are calculating the area of Circle.")
-            params = input("Please enter the radius of the circle:")
-            validated_params = validate_input(params.split())
-            if validated_params and len(validated_params) == 1:
-                circle = Circle(validated_params[0])
-                print("The area of circle with radius {} is {}".format(circle.radius, circle.calculate_area()))
-            else:
-                print("Please enter the correct number of parameters.")
-                print()
+            validated_params = get_validated_input("Please enter the radius of the circle (in ft):", 1)
+            circle = Circle(validated_params[0])
+            print("The area of circle with radius {} is {} sq ft.".format(circle.radius, circle.calculate_area()))
+            print()
                 
         elif option == 'e':
             print("We are calculating the area of Parallelogram.")
-            params = input("Please enter the base and height of the parallelogram separated by space:")
-            validated_params = validate_input(params.split())
-            if validated_params and len(validated_params) == 2:
-                parallelogram = Parallelogram(validated_params[0], validated_params[1])
-                print("The area of parallelogram with base {} and height {} is {}".format(parallelogram.base, parallelogram.height, parallelogram.calculate_area()))
-            else:
-                print("Please enter the correct number of parameters.")
-                print()
+            validated_params = get_validated_input("Please enter the base and height of parallelogram separated by space (in ft):", 2)
+            parallelogram = Parallelogram(validated_params[0], validated_params[1])
+            print("The area of parallelogram with base {} and height {} is {} sq ft.".format(parallelogram.base, parallelogram.height, parallelogram.calculate_area()))
+            print()
 
         elif option == 'f':
             print("Exiting the program.")
